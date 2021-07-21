9f70417e42d3f4eb6b6ea90cb984b3ea9a1ddba7
andreimoldovan23 <moldovanandrei2301@gmail.com>
2021-07-21 22:00:00 +0300

    Changed endpoint url

--- src/main/java/sfmc/beerinventory/web/controllers/BeerInventoryController.java
+++ src/main/java/sfmc/beerinventory/web/controllers/BeerInventoryController.java
@@ -21,7 +21,7 @@ public class BeerInventoryController {
     private final BeerInventoryRepository beerInventoryRepository;
     private final BeerInventoryMapper beerInventoryMapper;
 
-    @GetMapping("/api/v1/beer/{beerId}/inventory")
+    @GetMapping("api/v1/beer/{beerId}/inventory")
     List<BeerInventoryDTO> listBeersById(@PathVariable UUID beerId){
         log.trace("Finding Inventory for beerId: {}", beerId);
 
